23995e212913adb74da929f927c834dcd1d1aed0
Mori Asano <moasano@cisco.com>
2022-05-09 19:34:45 -0400

    animated button

---
+++ src/components/AnimatedButton.js
@@ -0,0 +1,13 @@
+import styles from "../styles/AnimatedButton.css"
+
+function AnimatedButton() {
+    return (
+        <div className={styles}>
+            <div class="buttonWrapper">
+                <a href="#" class="button">MORI</a>
+            </div>
+        </div>
+    );
+}
+
+export default AnimatedButton;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
+import AnimatedButton from './AnimatedButton';
 
 function Nav() {
     return (
         <div>
             <div style={{height: "20vh", backgroundColor: "rgba(0, 0, 0, 0.5)"}}>
-            MORI MORI MORI
+            <AnimatedButton></AnimatedButton>
             </div>
         </div>
     );
